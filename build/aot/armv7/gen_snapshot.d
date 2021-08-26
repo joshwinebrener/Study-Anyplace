9ee2e1a808cb827d124efdd098e8997b67ae81df
fantasticMan1 <joshua.winebrener@gmail.com>
2021-08-26 16:55:52 -0500

    Add .gitignore

--- build/aot/armv7/gen_snapshot.d
+++
@@ -1,1 +0,0 @@
-gen_snapshot.d: /Users/joshwinebrener/my_code/flutter/bin/cache/artifacts/engine/ios-release/gen_snapshot
--- build/frontend_server.d
+++
@@ -1,1 +0,0 @@
-frontend_server.d: /Users/joshwinebrener/my_code/flutter/bin/cache/artifacts/engine/darwin-x64/frontend_server.dart.snapshot
